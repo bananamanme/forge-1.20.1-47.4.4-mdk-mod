c361f65280d63c9e253500ee3873652e45d03390
bananamanme <roey0701@gmail.com>
2025-09-24 11:46:36 +0300

    bla

--- src/main/java/net/bananaman/it_starts_with_magic/entity/models/SacrificeArrowModel.java
+++ src/main/java/net/bananaman/it_starts_with_magic/entity/models/SacrificeArrowModel.java
@@ -1,7 +1,17 @@
-// Made with Blockbench 4.12.6
+package net.bananaman.it_starts_with_magic.entity.models;// Made with Blockbench 4.12.6
 // Exported for Minecraft version 1.17 or later with Mojang mappings
 // Paste this class into your mod and generate all required imports
 
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.minecraft.client.model.EntityModel;
+import net.minecraft.client.model.geom.ModelLayerLocation;
+import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.client.model.geom.PartPose;
+import net.minecraft.client.model.geom.builders.*;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.Entity;
 
 public class SacrificeArrowModel<T extends Entity> extends EntityModel<T> {
 	// This layer location should be baked with EntityRendererProvider.Context in the entity renderer and passed into this model's constructor
